774fc3836ae3fc63ec3402effc9f95277e77b454
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-05-04 15:03:35 +0500

    Create change controller

--- Company.UI/Company.UI/Controllers/EmployeeController.cs
+++ Company.UI/Company.UI/Controllers/EmployeeController.cs
@@ -3,25 +3,50 @@ using Domen.EmtityDTO.EmployeeDto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Company.UI.Controllers
+namespace Company.UI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeeController : ControllerBase
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class EmployeeController : ControllerBase
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeeController(IEmployeeService employeeService)
     {
-        private readonly IEmployeeService _employeeService  ;
+        _employeeService = employeeService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetEmployees()
+    {
+        List<EmployeeGetDto> employees = await _employeeService.GetAllEmployeesAsync();
+        return Ok(employees);
+    }
 
-        public EmployeeController(IEmployeeService employeeService)
-        {
-            _employeeService = employeeService;
-        }
-        [HttpGet]
-        public async Task<IActionResult> GetEmployees()
-        {
-            List<EmployeeGetDto> employees =await  _employeeService.GetAllEmployeesAsync();
-            return Ok(employees); 
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdEmployee(int id)
+    {
+        var employee = await _employeeService.GetByIdEmployeeAsync(id);
+        return Ok(employee);
+    }
 
+    [HttpPost]
+    public async Task<IActionResult> CreateEmployee(EmployeeCreateDto employeeCreateDto)
+    {
+        var employe = await _employeeService.CreateEmployeeAsync(employeeCreateDto);
+        return Ok(employe);
+    }
+
+
+    [HttpDelete]
+    public async Task<bool> DeleteEmoployee(int Id)
+    {
+        if (Id != 0)
+        {
+            return await _employeeService.DeleteEmployeeAsync(Id);
+        }
+        return false;
 
     }
+
+
 }
