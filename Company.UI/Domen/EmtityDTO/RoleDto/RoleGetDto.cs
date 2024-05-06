ff77d11041cd2903b9486728ef746de4df4813e6
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-05-06 20:32:15 +0500

    sdsd

--- Company.UI/Domen/EmtityDTO/RoleDto/RoleGetDto.cs
+++ Company.UI/Domen/EmtityDTO/RoleDto/RoleGetDto.cs
@@ -10,7 +10,8 @@ namespace Domen.EmtityDTO.RoleDto
     {
         public int Id { get; set; }
         public string FullName { get; set; } = null!;
- 
-
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
