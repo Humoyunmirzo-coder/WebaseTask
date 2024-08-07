5c0dddd4f8dffb3d0bef8b78130e061eeb63580d
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-08-07 13:50:37 +0500

    zx

--- Company.UI/Domen/EmtityDTO/OrganizationDto/OrganizationCreateDto.cs
+++ Company.UI/Domen/EmtityDTO/OrganizationDto/OrganizationCreateDto.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domen.EmtityDTO.OrganizationDto
+﻿namespace Domen.EmtityDTO.OrganizationDto
 {
-    public class OrganizationCreateDto :OrganizationBaseDto
+    public class OrganizationCreateDto : OrganizationBaseDto
     {
-       
+
         public string Name { get; set; } = null!;
         public string Address { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
