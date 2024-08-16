f8ce7397046e729ce0a79c4aba1b0a53df9c00c3
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-08-16 18:12:37 +0500

    asd

--- Company.UI/Domen/EmtityDTO/ProjectDto/ProjectUpdateDto.cs
+++ Company.UI/Domen/EmtityDTO/ProjectDto/ProjectUpdateDto.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domen.EmtityDTO.ProjectDto
+﻿namespace Domen.EmtityDTO.ProjectDto
 {
-    public  class ProjectUpdateDto : ProjectBaseDto
+    public class ProjectUpdateDto : ProjectBaseDto
     {
         public int Id { get; set; }
     }
