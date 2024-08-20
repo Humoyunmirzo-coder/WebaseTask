097290869078256f52f7fb1e04f1e78bcdf6bcd3
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-08-20 20:57:56 +0500

    asd

--- Company.UI/Domen/EmtityDTO/RoleDto/RoleGetDto.cs
+++ Company.UI/Domen/EmtityDTO/RoleDto/RoleGetDto.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domen.EmtityDTO.RoleDto
+﻿namespace Domen.EmtityDTO.RoleDto
 {
-    public  class RoleGetDto : RoleBaseDto
+    public class RoleGetDto : RoleBaseDto
     {
         public int Id { get; set; }
         public string FullName { get; set; } = null!;
         public string? Description { get; set; }
-   
+
     }
 }
