3cec88524497ab35649e3aecfe8300ae5f3fbf41
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-08-20 20:57:07 +0500

    123

--- Company.UI/Domen/EmtityDTO/RoleDto/RoleCreateDto.cs
+++ Company.UI/Domen/EmtityDTO/RoleDto/RoleCreateDto.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domen.EmtityDTO.RoleDto
+﻿namespace Domen.EmtityDTO.RoleDto
 {
-    public  class RoleCreateDto : RoleBaseDto
+    public class RoleCreateDto : RoleBaseDto
     {
         public string FullName { get; set; } = null!;
         public string? Description { get; set; }
