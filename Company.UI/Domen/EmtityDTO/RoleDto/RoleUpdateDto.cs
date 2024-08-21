3059e89e5418390475c1994521cd4b5c5a68f36c
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-08-21 16:50:43 +0500

    cleane excess code

--- Company.UI/Domen/EmtityDTO/RoleDto/RoleUpdateDto.cs
+++ Company.UI/Domen/EmtityDTO/RoleDto/RoleUpdateDto.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domen.EmtityDTO.RoleDto
+﻿namespace Domen.EmtityDTO.RoleDto
 {
-    public  class RoleUpdateDto : RoleBaseDto
+    public class RoleUpdateDto : RoleBaseDto
     {
         public int Id { get; set; }
     }
