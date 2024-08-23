54b25f09f507d932579d30934d3cd7ae81c56444
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-08-23 10:42:34 +0500

    123

--- Company.UI/Domen/EmtityDTO/TaskDto/TaskGetDto.cs
+++ Company.UI/Domen/EmtityDTO/TaskDto/TaskGetDto.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domen.EmtityDTO.TaskDto
+﻿namespace Domen.EmtityDTO.TaskDto
 {
-    public  class TaskGetDto : TaskBaseDto
+    public class TaskGetDto : TaskBaseDto
     {
         public int Id { get; set; }
         public int? ProjectId { get; set; }
